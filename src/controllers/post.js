14f23a462cb90a0c492ba1145f494f7682a43b8a
Lanlyhs <ziqiang.chen@outlook.com>
2020-04-24 10:36:33 +0800

    refactoring: remove try catch

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -2,97 +2,57 @@ const Posts = require("../models/Posts");
 
 module.exports = {
     List: async (ctx) => {
-        try {
-            const posts = await Posts.query().select();
+        const posts = await Posts.query().select();
 
-            ctx.status = 200;
-            ctx.body = {
-                data: posts,
-            };
-        } catch (e) {
-            const message = "Internal server error";
-            ctx.status = e.statusCode || 500;
-            ctx.body = {
-                error: e.data || { message },
-            };
-        }
+        ctx.status = 200;
+        ctx.body = {
+            data: posts,
+        };
     },
     View: async (ctx) => {
-        try {
-            const { id } = ctx.params;
+        const { id } = ctx.params;
 
-            const [post] = await Posts.query().select().where({ id });
-            ctx.status = 200;
-            ctx.body = { data: post };
-        } catch (e) {
-            const message = "Internal server error";
-            ctx.status = e.statusCode || 500;
-            ctx.body = {
-                error: e.data || { message },
-            };
-        }
+        const [post] = await Posts.query().select().where({ id });
+        ctx.status = 200;
+        ctx.body = { data: post };
     },
     Create: async (ctx) => {
-        try {
-            const { title, content } = ctx.request.body;
+        const { title, content } = ctx.request.body;
 
-            const post = await Posts.query().insert({ title, content });
+        const post = await Posts.query().insert({ title, content });
 
-            ctx.status = 200;
-            ctx.body = { data: post };
-        } catch (e) {
-            const message = "Internal server error";
-            ctx.status = e.statusCode || 500;
-            ctx.body = {
-                error: e.data || { message },
-            };
-        }
+        ctx.status = 200;
+        ctx.body = { data: post };
     },
     Patch: async (ctx) => {
-        try {
-            const { id } = ctx.params;
-            const { status } = ctx.request.body;
+        const { id } = ctx.params;
+        const { status } = ctx.request.body;
 
-            const affectedRows = await Posts.query()
-                .patch({ status })
-                .where({ id });
+        const affectedRows = await Posts.query()
+            .patch({ status })
+            .where({ id });
 
-            if (!affectedRows) {
-                ctx.throw(409, { data: { message: "Conflict patch posts." } });
-            }
-
-            ctx.status = 200;
-            ctx.body = {
-                data: { status },
-            };
-        } catch (e) {
-            const message = "Internal server error";
-            ctx.status = e.statusCode || 500;
-            ctx.body = {
-                error: e.data || { message },
-            };
+        if (!affectedRows) {
+            ctx.throw(409, { data: { message: "Conflict patch posts." } });
         }
+
+        ctx.status = 200;
+        ctx.body = {
+            data: { status },
+        };
     },
     Delete: async (ctx) => {
-        try {
-            const { id } = ctx.params;
+        const { id } = ctx.params;
 
-            const affectedRows = await Posts.query().delete().where({ id });
+        const affectedRows = await Posts.query().delete().where({ id });
 
-            if (!affectedRows) {
-                ctx.throw(409, { data: { message: "Conflict delete post" } });
-            }
-
-            ctx.status = 200;
-            ctx.body = {
-                message: "success",
-            };
-        } catch (e) {
-            const message = "Internal server error";
-            ctx.status = e.statusCode || 500;
-            ctx.body = {
-                error: e.data || { message },
-            };
+        if (!affectedRows) {
+            ctx.throw(409, { data: { message: "Conflict delete post" } });
         }
+
+        ctx.status = 200;
+        ctx.body = {
+            message: "success",
+        };
     },
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,50 @@
-const Koa = require('koa');
-const bodyParser = require('koa-body');
-const router = require('./routes');
-
+const Koa = require("koa");
+const app = new Koa();
+const koaBody = require("koa-body");
+const router = require("./routes");
 const PORT = process.env.PORT || 8080;
 
-module.exports = new Koa()
-  .use(bodyParser())
-  .use(router.routes()) // Attach posts entity router
-  .listen(PORT, () => console.log(`Simple service listening in port: ${PORT}`));
+// error handeling
+app.use(async (ctx, next) => {
+    try {
+        await next();
+    } catch (err) {
+        const message = "Internal server error";
+        ctx.status = err.statusCode || 500;
+        ctx.body = {
+            error: err.data || { message },
+        };
+    }
+});
+
+// error log, centralized error handling
+app.on("error", async (err, ctx) => {
+    console.error("server error", err, ctx);
+});
+
+// logger
+app.use(async (ctx, next) => {
+    await next();
+    const rt = ctx.response.get("X-Response-Time");
+    console.log(`${ctx.method} ${ctx.url} - ${rt}`);
+});
+
+// x-response-time
+app.use(async (ctx, next) => {
+    const start = Date.now();
+    await next();
+    const ms = Date.now() - start;
+    ctx.set("X-Response-Time", `${ms}ms`);
+});
+
+// use koa-body
+app.use(koaBody());
+
+// connect router
+app.use(router.routes());
+
+const server = app.listen(PORT, () =>
+    console.log(`Simple service listening in port: ${PORT}`)
+);
+
+module.exports = server;
